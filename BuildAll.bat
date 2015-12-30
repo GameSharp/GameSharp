f21ad144f6c51e751363c66393b1c160f189c8f4
Ivan K <to4ka.tut@bk.ru>
2015-12-31 01:31:16 +0400

    Create Bin dir when building

--- BuildAll.bat
+++ BuildAll.bat
@@ -1,4 +1,5 @@
 set "PATH=c:\Windows\Microsoft.NET\Framework\v3.5\"
+mkdir Bin
 csc /target:library /out:Bin\GameSharp.dll Source\GameSharp\*.cs
 csc /target:exe /out:Bin\HelloWorld.exe /reference:Bin\GameSharp.dll Source\Samples\01_HelloWorld\*.cs
 pause
