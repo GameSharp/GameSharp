bbb0e71ef4a78e5852e3d063928fd5011f0ff0ae
Ivan K <to4ka.tut@bk.ru>
2015-12-30 22:12:47 +0400

    Add main loop

--- Source/GameSharp/Game.cs
+++ Source/GameSharp/Game.cs
@@ -1,11 +1,29 @@
+using System;
+
 namespace GameSharp
 {
     public class Game
     {
+        bool isExiting = false;
+        
+        public void Exit()
+        {
+            isExiting = true;
+        }
+
         public void Run()
         {
-            System.Console.WriteLine("Hello World");
-            System.Console.ReadKey();
+            while (!isExiting)
+            {
+                Console.WriteLine("Press Esc");
+                while (Console.KeyAvailable)
+                {
+                    var cki = Console.ReadKey(true);
+                    if (cki.Key == ConsoleKey.Escape)
+                        Exit();
+                }
+                System.Threading.Thread.Sleep(200);
+            }
         }
     }
 }
