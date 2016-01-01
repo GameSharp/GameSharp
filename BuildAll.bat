2fad82e5bb90890873f0bfa7291d5b4dcefc30ad
Ivan K <to4ka.tut@bk.ru>
2016-01-01 13:12:04 +0400

    Beautiful BuildAll.bat

--- BuildAll.bat
+++ BuildAll.bat
@@ -1,4 +1,11 @@
 set "PATH=c:\Windows\Microsoft.NET\Framework\v4.0.30319\"
-csc /unsafe /platform:x86 /target:library /out:Bin\GameSharp.dll Source\GameSharp\*.cs Source\GameSharp\Input\*.cs Source\ThirdParty\SDL2-CS\src\*.cs Source\ThirdParty\opengl4csharp\OpenGL\*.cs
+
+csc /unsafe /platform:x86 /target:library /out:Bin\GameSharp.dll^
+    Source\GameSharp\*.cs^
+    Source\GameSharp\Input\*.cs^
+    Source\ThirdParty\SDL2-CS\src\*.cs^
+    Source\ThirdParty\opengl4csharp\OpenGL\*.cs
+
 csc /platform:x86 /target:winexe /out:Bin\HelloWorld.exe /reference:Bin\GameSharp.dll Source\Samples\01_HelloWorld\*.cs
+
 pause
